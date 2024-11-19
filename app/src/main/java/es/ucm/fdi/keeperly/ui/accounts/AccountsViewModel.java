09b77289ee6d08b783b2e5d944f3e8629702e234
IvanAlcCam <ivanalca@ucm.es>
2024-11-20 00:14:19 +0100

    Creado Menu de navegacion

---
+++ app/src/main/java/es/ucm/fdi/keeperly/ui/accounts/AccountsViewModel.java
@@ -0,0 +1,19 @@
+package es.ucm.fdi.keeperly.ui.accounts;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class AccountsViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public AccountsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is accounts fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}

b3308650b67303d6efec6954ad4238da62569ba0
Melendo <nachomelendo@gmail.com>
2024-11-27 17:05:15 +0100

    Creado los fragments para el inicio y avanzado con el inicioFragment e InicioViewModel

--- app/src/main/java/es/ucm/fdi/keeperly/ui/inicio/InicioFragment.java
+++ app/src/main/java/es/ucm/fdi/keeperly/ui/inicio/InicioFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import es.ucm.fdi.keeperly.databinding.FragmentInicioBinding;
 
@@ -16,16 +18,25 @@ public class InicioFragment extends Fragment {
 
     private FragmentInicioBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        InicioViewModel homeViewModel =
-                new ViewModelProvider(this).get(InicioViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        InicioViewModel inicioViewModel = new ViewModelProvider(this).get(InicioViewModel.class);
 
         binding = FragmentInicioBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView welcomeText = binding.welcomeText;
+        inicioViewModel.getWelcomeText().observe(getViewLifecycleOwner(), welcomeText::setText);
+        final TextView priceThisMonth = binding.priceThisMonth;
+        inicioViewModel.getPriceThisMonth().observe(getViewLifecycleOwner(), priceThisMonth::setText);
+        final TextView priceLastMonth = binding.priceLastMonth;
+        inicioViewModel.getPriceLastMonth().observe(getViewLifecycleOwner(), priceLastMonth::setText);
+        final ProgressBar progressBarThisMonth = binding.progressBarThisMonth;
+        inicioViewModel.getProgressBarThisMonth().observe(getViewLifecycleOwner(), progressBarThisMonth::setProgress);
+        final ProgressBar progressBarLastMonth = binding.progressBarLastMonth;
+        inicioViewModel.getProgressBarLastMonth().observe(getViewLifecycleOwner(), progressBarLastMonth::setProgress);
+        final RecyclerView recyclerViewCuentas = binding.recyclerViewCuentas;
+        final RecyclerView recyclerViewPresupuestos = binding.recyclerViewPresupuestos;
+
         return root;
     }
 
--- app/src/main/java/es/ucm/fdi/keeperly/ui/inicio/InicioViewModel.java
+++ app/src/main/java/es/ucm/fdi/keeperly/ui/inicio/InicioViewModel.java
@@ -6,14 +6,45 @@ import androidx.lifecycle.ViewModel;
 
 public class InicioViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<String> welcomeText;
+    private final MutableLiveData<String> priceThisMonth;
+    private final MutableLiveData<String> priceLastMonth;
+    private final MutableLiveData<Integer> progressBarThisMonth;
+    private final MutableLiveData<Integer> progressBarLastMonth;
 
     public InicioViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        welcomeText = new MutableLiveData<>();
+        priceThisMonth = new MutableLiveData<>();
+        priceLastMonth = new MutableLiveData<>();
+        progressBarThisMonth = new MutableLiveData<>();
+        progressBarLastMonth = new MutableLiveData<>();
+
+
+        welcomeText.setValue("Bienvenido, Iván");
+        priceThisMonth.setValue("69.42€");
+        priceLastMonth.setValue("123.45€");
+        progressBarThisMonth.setValue(45);
+        progressBarLastMonth.setValue(80);
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<String> getWelcomeText() {
+        return welcomeText;
     }
+
+    public LiveData<String> getPriceThisMonth() {
+        return priceThisMonth;
+    }
+
+    public LiveData<String> getPriceLastMonth() {
+        return priceLastMonth;
+    }
+
+    public LiveData<Integer> getProgressBarThisMonth() {
+        return progressBarThisMonth;
+    }
+
+    public LiveData<Integer> getProgressBarLastMonth() {
+        return progressBarLastMonth;
+    }
+
 }

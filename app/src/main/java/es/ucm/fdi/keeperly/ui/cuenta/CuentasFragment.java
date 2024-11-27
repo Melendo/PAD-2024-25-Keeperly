b2247935fbf02981b4e59a18441485fd6d2844a7
IvanAlcCam <alccamdev@gmail.com>
2024-11-27 12:47:30 +0100

    Carga el fragment de CrearCuenta

--- app/src/main/java/es/ucm/fdi/keeperly/ui/cuenta/CuentasFragment.java
+++ app/src/main/java/es/ucm/fdi/keeperly/ui/cuenta/CuentasFragment.java
@@ -1,4 +1,4 @@
-package es.ucm.fdi.keeperly.ui.cuentas;
+package es.ucm.fdi.keeperly.ui.cuenta;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,7 +9,12 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import es.ucm.fdi.keeperly.R;
 import es.ucm.fdi.keeperly.databinding.FragmentCuentasBinding;
 
 
@@ -24,6 +29,14 @@ public class CuentasFragment extends Fragment {
 
         binding = FragmentCuentasBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        FloatingActionButton fabCrearCuenta = root.findViewById(R.id.fab_crear_cuenta);
+
+        fabCrearCuenta.setOnClickListener(v -> {
+            // Navegación hacia el formulario de creación de presupuesto
+            NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_menu);
+            navController.navigate(R.id.action_cuentasFragment_to_crearCuentaFragment);
+        });
 
         final TextView textView = binding.textAccounts;
         accountsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);

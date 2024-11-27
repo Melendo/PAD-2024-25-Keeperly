49836815034fbe6418d9cdee80f49d85aa257a3b
Melendo <nachomelendo@gmail.com>
2024-11-27 19:10:57 +0100

    Preparado para sacar el Dinero total de las cuentas y el total gastado

--- app/src/main/java/es/ucm/fdi/keeperly/ui/inicio/InicioViewModel.java
+++ app/src/main/java/es/ucm/fdi/keeperly/ui/inicio/InicioViewModel.java
@@ -4,16 +4,26 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import es.ucm.fdi.keeperly.data.local.database.entities.Cuenta;
+import es.ucm.fdi.keeperly.repository.CuentaRepository;
 import es.ucm.fdi.keeperly.repository.LoginRepository;
 import es.ucm.fdi.keeperly.repository.RepositoryFactory;
+import es.ucm.fdi.keeperly.repository.TransaccionRepository;
 
 public class InicioViewModel extends ViewModel {
 
     private final MutableLiveData<String> welcomeText;
     private final MutableLiveData<String> numDineroTotal;
-    private final MutableLiveData<String> numTotalGastado;;
+    private final MutableLiveData<String> numTotalGastado;
+
 
     private final LoginRepository loginRepository;
+    //private final CuentaRepository cuentaRepository;
+    //private final TransaccionRepository transaccionRepository;
+
+    //private final List<Cuenta> cuentas>;
+    //private final List<Transaccion> transacciones>;
+
 
     public InicioViewModel() {
         welcomeText = new MutableLiveData<>();
@@ -21,6 +31,9 @@ public class InicioViewModel extends ViewModel {
         numTotalGastado = new MutableLiveData<>();
 
         loginRepository = LoginRepository.getInstance(RepositoryFactory.getInstance().getUsuarioRepository());
+
+        //cuentasRepository = CuentaRepository.getInstance(RepositoryFactory.getInstance().getCuentaRepository());
+        //transaccionRepository = TransaccionRepository.getInstance(RepositoryFactory.getInstance().getTransaccionRepository());
 
 
         welcomeText.setValue("Hola, " + loginRepository.getUser().getNombre());

8a307d23e1f31ff41d4716862e5804e295ac6baf
Melendo <nachomelendo@gmail.com>
2024-11-30 17:34:17 +0100

    El getCuentas otra vez

--- app/src/main/java/es/ucm/fdi/keeperly/ui/cuenta/CuentasViewModel.java
+++ app/src/main/java/es/ucm/fdi/keeperly/ui/cuenta/CuentasViewModel.java
@@ -38,4 +38,7 @@ public class CuentasViewModel extends ViewModel {
         return cuentaRepository.getOperationStatus();
     }
 
+    public LiveData<List<Cuenta>> getCuentas() {
+        return cuentas;
+    }
 }

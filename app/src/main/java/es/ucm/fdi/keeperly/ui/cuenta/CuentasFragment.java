0b40ff8bc224c9a27f2950e35b058fe191000a92
Melendo <nachomelendo@gmail.com>
2024-12-02 17:43:49 +0100

    Configurada conexion a internet para la API

--- app/src/main/java/es/ucm/fdi/keeperly/ui/cuenta/CuentasFragment.java
+++ app/src/main/java/es/ucm/fdi/keeperly/ui/cuenta/CuentasFragment.java
@@ -35,27 +35,13 @@ public class CuentasFragment extends Fragment {
     private CuentasViewModel cuentasViewModel;
     private CuentasAdapter cuentasAdapter;
 
-    private ConnectivityManager connectivityManager;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         cuentasViewModel = new ViewModelProvider(this).get(CuentasViewModel.class);
 
         binding = FragmentCuentasBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        //Obtenemos una instancia del ConnectivityManager
-        connectivityManager = (ConnectivityManager) requireContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        Network network = connectivityManager.getActiveNetwork();
-        NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
-
-        if (networkCapabilities != null && networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)) {
-            // Hay conexión a internet
-
-        } else {
-            // No hay conexión a internet
-
-        }
 
         RecyclerView recyclerView = root.findViewById(R.id.recyclerViewCuentas);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -88,27 +74,7 @@ public class CuentasFragment extends Fragment {
     }
 
 
-    private ConnectivityManager.NetworkCallback networkCallback = new ConnectivityManager.NetworkCallback() {
-        @Override
-        public void onAvailable(@NonNull Network network) {
-            super.onAvailable(network);
-            // La red está disponible, puedes reintentar la llamada a la API de PayPal si es necesario
-        }
 
-        @Override
-        public void onLost(@NonNull Network network) {super.onLost(network);
-            // La red se ha perdido, muestra un mensaje de error al usuario
-        }
-
-        @Override
-        public void onCapabilitiesChanged(@NonNull Network network,
-                                          @NonNull NetworkCapabilities networkCapabilities)
-        {
-            super.onCapabilitiesChanged(network, networkCapabilities);
-            final boolean unmetered = networkCapabilities.hasCapability
-                    (NetworkCapabilities.NET_CAPABILITY_NOT_METERED);
-        }
-    };
 
     @Override
     public void onDestroyView() {

06cfd6fe8fca747f187fedc481e2b3e164d4e658
Melendo <nachomelendo@gmail.com>
2024-12-02 13:10:49 +0100

    Configuración de internet para API paypal

--- app/src/main/java/es/ucm/fdi/keeperly/ui/cuenta/CuentasFragment.java
+++ app/src/main/java/es/ucm/fdi/keeperly/ui/cuenta/CuentasFragment.java
@@ -1,5 +1,9 @@
 package es.ucm.fdi.keeperly.ui.cuenta;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,11 +35,27 @@ public class CuentasFragment extends Fragment {
     private CuentasViewModel cuentasViewModel;
     private CuentasAdapter cuentasAdapter;
 
+    private ConnectivityManager connectivityManager;
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         cuentasViewModel = new ViewModelProvider(this).get(CuentasViewModel.class);
 
         binding = FragmentCuentasBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        //Obtenemos una instancia del ConnectivityManager
+        connectivityManager = (ConnectivityManager) requireContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        Network network = connectivityManager.getActiveNetwork();
+        NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+
+        if (networkCapabilities != null && networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)) {
+            // Hay conexión a internet
+
+        } else {
+            // No hay conexión a internet
+
+        }
 
         RecyclerView recyclerView = root.findViewById(R.id.recyclerViewCuentas);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -165,6 +185,28 @@ public class CuentasFragment extends Fragment {
         //Muestra el dialogo
         dialog.show();
     }
+
+    private ConnectivityManager.NetworkCallback networkCallback = new ConnectivityManager.NetworkCallback() {
+        @Override
+        public void onAvailable(@NonNull Network network) {
+            super.onAvailable(network);
+            // La red está disponible, puedes reintentar la llamada a la API de PayPal si es necesario
+        }
+
+        @Override
+        public void onLost(@NonNull Network network) {super.onLost(network);
+            // La red se ha perdido, muestra un mensaje de error al usuario
+        }
+
+        @Override
+        public void onCapabilitiesChanged(@NonNull Network network,
+                                          @NonNull NetworkCapabilities networkCapabilities)
+        {
+            super.onCapabilitiesChanged(network, networkCapabilities);
+            final boolean unmetered = networkCapabilities.hasCapability
+                    (NetworkCapabilities.NET_CAPABILITY_NOT_METERED);
+        }
+    };
 
     @Override
     public void onDestroyView() {
